dcd4d8467af3d9d0fc78a143cc60bb54f89db72a
M40 <modev.404@gmail.com>
2025-05-10 13:54:25 +0300

    load evn and Setup and test gemini ai

--- app.py
+++ app.py
@@ -1,1 +1,21 @@
+from dotenv import  load_dotenv
+import os
+import  google.generativeai as genai
 
+load_dotenv()
+
+# set up gemini AI
+genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+ai = genai.GenerativeModel("gemini-2.0-flash")
+
+
+def test():
+    try:
+        response = ai.generate_content("What is your name?")
+        return response.text.strip()
+    except Exception as e:
+        return f"Error: {e}"
+
+
+if __name__ == "__main__":
+    print(test())
