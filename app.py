3a371d0c8d6be95af58c3ebeb18c345f85d597cc
M40 <modev.404@gmail.com>
2025-05-10 14:00:12 +0300

    Load env API keys for X(twitter)

--- app.py
+++ app.py
@@ -4,8 +4,19 @@ import  google.generativeai as genai
 
 load_dotenv()
 
-# set up gemini AI
-genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+# environment variables
+
+# X API KEYS
+X_API_KEY= os.getenv("X_API_KEY")
+X_API_SECRET = os.getenv("X_API_SECRET")
+X_ACCESS_TOKEN = os.getenv("X_ACCESS_TOKEN")
+X_ACCESS_SECRET = os.getenv("X_ACCESS_SECRET")
+
+# Gemini AI KEYS
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+
+# setup gemini AI
+genai.configure(api_key=GEMINI_API_KEY)
 ai = genai.GenerativeModel("gemini-2.0-flash")
 
 
